0598281caa61850f82e657d9402565edad4427c7
newkayak12 <newkayak12@gmail.com>
2021-08-26 19:20:06 +0900

    yj Portfolio ServerDeploy

---
+++ portfolio/src/test/java/com/portfolio/yj/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.yj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
