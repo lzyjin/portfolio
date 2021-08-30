936e22addb7527a6a050706429ab979d75a17bb1
lzyjin <1247y.j@gmail.com>
2021-08-30 18:01:43 +0900

    yj

---
+++ portfolio/src/main/webapp/cgv/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <MovieSlide></MovieSlide>
+    <MovieChart></MovieChart>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import MovieSlide from './components/MovieSlide.vue'
+import MovieChart from './components/MovieChart.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  components : {
+    'Header' : Header,
+    'MovieSlide' : MovieSlide,
+    'MovieChart' : MovieChart,
+    'Footer' : Footer
+  }
+}
+</script>
+
+<style>
+
+</style>
